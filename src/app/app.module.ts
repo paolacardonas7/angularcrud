d5142b3b40d460ccac3469fd25adb2b740fdb00b
Paola Andrea Cardona Salcedo <pao_cardona97@hotmail.com>
2020-12-18 12:14:43 -0500

    service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrudAddComponent } from './crud-add/crud-add.component';
 import { CrudGetComponent } from './crud-get/crud-get.component';
 import { CrudEditComponent } from './crud-edit/crud-edit.component';
-
+import { BusinessService } from './business.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +17,13 @@ import { CrudEditComponent } from './crud-edit/crud-edit.component';
     CrudEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BusinessService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
